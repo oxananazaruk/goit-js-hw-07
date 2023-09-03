00b0102bb0b4038fc45dbdc408c5920a2785881c
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-09-03 08:41:26 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
 
 const markup = createMarkup(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', markup);
 
-galleryEl.addEventListener('clikc', onClickImageHandle);
+galleryEl.addEventListener('click', onClickImageHandle);
 
 function createMarkup(array) {
     return array.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
+        return `<li data-description="${description}" class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -26,12 +26,20 @@ function createMarkup(array) {
 };
 
 function onClickImageHandle(event) {
-    // event.stopPropagation();
+  event.preventDefault();
     if (event.target === event.currentTarget) {
         return;
     }
 
-    const targetElement = event.target.closest('.gallery__image');
-    console.log(targetElement);
-    // const imageName = 
+  const targetElement = event.target.closest('.gallery__item');
+  const imageName = targetElement.dataset.description;
+  const imageInfo = galleryItems.find(img => img.description === imageName);
+ 
+  const instance = basicLightbox.create(`
+  <div class="modal">
+      <img src="${imageInfo.original}" alt="${imageInfo.description}" width="800" height="600" />
+      </div>
+   `);
+
+  instance.show();
 };
